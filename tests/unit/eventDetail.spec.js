d80ab7cc89a4d7f385d7dca7ec5f5e8f4d92b12e
madokafujimakiahlsen <madoka.fujimakiahlsen@gmail.com>
2020-09-21 21:30:32 +0200

    red test for EventDetail.vue

---
+++ tests/unit/eventDetail.spec.js
@@ -0,0 +1,47 @@
+import { shallowMount, mount, createLocalVue } from "@vue/test-utils";
+import EventDetail from "@/components/EventDetail.vue";
+import Vuex from "vuex";
+
+const localVue = createLocalVue();
+localVue.use(Vuex);
+
+describe("EventDetail.vue", () => {
+  let wrapper;
+  let store;
+  let storeOptions;
+  let event = {
+    id: 0,
+    title: "Höstens picknick",
+    place: "Vasaparken",
+    date: "2020-09-15",
+    image: "picnic.jpg",
+    description:
+      "Vivamus viverra, augue blandit ultricies euismod, justo nisl condimentum urna, nec sodales orci dolor vitae dolor. Donec at leo et velit faucibus egestas ut vulputate magna. Vivamus eu posuere dui. Etiam semper venenatis purus, quis laoreet est elementum nec.",
+    host: "1.jpg",
+    participant: [],
+    reviews: [],
+  };
+
+  beforeEach(() => {
+    storeOptions = {
+      state: { login: false, loginFormIsVisible: false },
+      actions: { checkLogin: jest.fn() },
+    };
+    store = new Vuex.Store(storeOptions);
+
+    wrapper = shallowMount(EventDetail, {
+      propsData: { event },
+      localVue,
+      store,
+    });
+  });
+
+  it("should calls store action 'checkLogin' when button 'Delta' is clicked", () => {
+    //Arrange
+    let button = wrapper.find("button");
+    //Act
+    button.trigger("click");
+    //Assert
+    expect(storeOptions.actions.checkLogin).toHaveBeenCalled();
+  });
+});
