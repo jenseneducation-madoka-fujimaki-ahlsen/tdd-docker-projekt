1fd5787205a6524cffb3ed28b9139013f9a36e4d
madokafujimakiahlsen <madoka.fujimakiahlsen@gmail.com>
2020-09-30 10:52:59 +0200

    change code in main.yml

--- nightwatch.conf.js
+++ nightwatch.conf.js
@@ -12,7 +12,7 @@ module.exports = {
       desiredCapabilities: {
         browserName: "chrome",
         chromeOptions: {
-          args: ["--headless", "--no-sandbox", "--disable-dev-shm-usage"],
+          args: ["--no-sandbox", "--disable-dev-shm-usage"],
         },
       },
     },
